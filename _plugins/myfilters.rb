c84ba05c17a6340a68b94a44af4ec19c3d0a77d3
Daniel Armengol Altayó <daniel@armengol-altayo.com>
2021-11-14 17:09:05 +0100

    update db

---
+++ _plugins/myfilters.rb
@@ -0,0 +1,11 @@
+# via https://stackoverflow.com/questions/14978474/how-to-show-the-modification-date-of-a-file-in-jekyll
+
+module Jekyll
+  module MyFilters
+    def file_mod_date(input)
+      File.mtime(input)
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::MyFilters)
